becdc124afa98eb1a96020f596e8848f31b0dfa6
ercchy <erika@serox.si>
2014-07-15 13:14:47 +0200

    added sidebar for docs

--- p2pu-bootstrap-3/javascripts/p2pustrap.js
+++ p2pu-bootstrap-3/javascripts/p2pustrap.js
@@ -9,7 +9,7 @@
 				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 				if (target.length) {
 					$('html,body').animate({
-						scrollTop: target.offset().top -190
+						scrollTop: target.offset().top -100
 					}, 1000);
 					return false;
 				}
