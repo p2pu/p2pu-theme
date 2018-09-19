9cf9ea52f127a0f87a654c624340b6a90a5984c8
Sharon <sharon.peishan.kennedy@gmail.com>
2018-09-19 19:41:36 -0400

    add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,16 @@ module.exports = {
           options: { sourceMap: true }
         }]
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      }
     ]
   },
   plugins: [
